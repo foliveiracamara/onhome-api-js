bd5ce22ad8a9c0eca486a2053b5301d64b168df4
gabrielsouzaexe <gabrielsouza628@gmail.com>
2022-05-19 09:01:02 -0300

    Ajustando porta

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 8080
 
 
 app.use(cors());
 app.use(express.json());
-app.listen(8080, () => console.log("Servidor iniciado"));
+app.listen(PORT, () => console.log("Servidor iniciado"));
 
 require('./src/routes')(app);
