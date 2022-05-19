604846f4bb59871fdab10da1d553837f94ec4b5b
gabrielsouzaexe <gabrielsouza628@gmail.com>
2022-05-19 09:03:58 -0300

    Retirando conexao local

--- src/config/database.js
+++ src/config/database.js
@@ -12,13 +12,9 @@ async function connect() {
     };
 
     // Organizar estrutura de envio
-
-    const mysql = require("mysql2/promise");
     const sqls = require("mssql");
-    const connection = await mysql.createConnection(`mysql://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:3306/onhome`);
     const connection2 = sqls.connect(config);
     global.connection = connection2;
-    // global.connection = connection;
     console.log("Conexão com banco efetuada com sucesso");
     return connection2;
 }
