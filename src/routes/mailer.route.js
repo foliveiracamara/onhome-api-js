8c6accc51699a54cd8b51d00cd5d07e5bd4d6213
gabrielsouzaexe <gabrielsouza628@gmail.com>
2022-05-19 23:50:32 -0300

    Ajustando server

--- src/routes/mailer.route.js
+++ src/routes/mailer.route.js
@@ -3,7 +3,6 @@ const nodemailerService = require('../services/nodemailer')
 module.exports = async (app) => {
     app.get('/mailer', async (req, res) => {
         const result = JSON.stringify(await nodemailerService.run())
-        console.log(result)
         res.status(200).send({result: `${result}`})
     });
 }
