e0b55fb393b0a686b1f676cbcc702ad9cd4a4a3c
gabrielsouzaexe <gabrielsouza628@gmail.com>
2022-05-28 07:10:14 -0300

    fix: liberando rota de processo

--- src/routes/processo.route.js
+++ src/routes/processo.route.js
@@ -1,6 +1,8 @@
 const processoController = require("../controller/processo.controller");
 const authenticatedRoute = require("../middleware/auth");
 
+// Adicionar autenticação
+
 module.exports = (app) => {
-    app.get('/processo/usuario/:id', authenticatedRoute, processoController.selecionarProcessosPorUsuario);
+    app.get('/processo/usuario/:id', processoController.selecionarProcessosPorUsuario);
 }
