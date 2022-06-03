9561ca33be7e7bb9fccd0c23ad62d6e9b5ca6997
gabrielsouzaexe <gabrielsouza628@gmail.com>
2022-06-03 09:48:57 -0300

    feat: adds user auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,9 +2,13 @@ const tb = require('../model')
 
 async function autenticarUsuario(email, senha) {
     const resultadoConsulta = await tb.usuario.selecionarUsuarioPorEmail(email, senha)
-    const [informacoesUsuario] = await resultadoConsulta.recordsets
-    if (informacoesUsuario.length > 0) {
-        return [true, informacoesUsuario]
+    const [[informacoesUsuario]] = await resultadoConsulta.recordsets
+    const { fkEmpresa } = informacoesUsuario
+    const primeiroAcesso = fkEmpresa === null ? true : false
+
+
+    if (informacoesUsuario.nomeUsuario !== null) {
+        return [true, informacoesUsuario, primeiroAcesso]
     } else {
         return [false, null]
     }
