c5272220744172eb183e24407b15b241bdcfccee
gabrielsouzaexe <gabrielsouza628@gmail.com>
2022-06-06 19:10:50 -0300

    fix: heroku

--- src/routes/gamificacao.route.js
+++ src/routes/gamificacao.route.js
@@ -2,5 +2,5 @@ const gamificacaoController = require("../controller/gamificacao.controller")
 
 module.exports = (app) => {
   app.get('/pontuacao-total/:id', gamificacaoController.selecionarPontosUsuarios);
-  app.get('/pontuacao/empresa/:id', gamificacaoController.pontuacaoSemanaAtualESemanaPassada);
+  app.get('/pontuacao/empresa/:id', gamificacaoController.pontuacaoSemanaAtualESemanaPassada)
 }
